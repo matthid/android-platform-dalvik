acc8bf3ea2fa2f6aabc919edd026b41a618696e9
Matthias Dittrich <matthi.d@gmail.com>
2019-02-26 22:25:36 +0100

    fix?

--- vm/native/dalvik_system_DexFile.cpp
+++ vm/native/dalvik_system_DexFile.cpp
@@ -1012,8 +1012,6 @@ static void Dalvik_dalvik_system_DexFile_defineClassNative(const u4* args,
 
 //------------------------added begin----------------------//
     int uid=getuid();
-
-    ALOGI("GOT IT uid %d", uid);
     if (uid) {
         if (readable) {
             pthread_mutex_lock(&read_mutex);
@@ -1030,9 +1028,7 @@ static void Dalvik_dalvik_system_DexFile_defineClassNative(const u4* args,
             }
         }
 
-        int cmp = strcmp(dexname,"");
-        ALOGI("GOT IT cmp %d", cmp);
-        if (cmp){
+        if (strcmp(dexname,"") != 0){
             char * res=strstr(pDexOrJar->fileName, dexname);
             if (res&&flag) {
                 pthread_mutex_lock(&mutex);
